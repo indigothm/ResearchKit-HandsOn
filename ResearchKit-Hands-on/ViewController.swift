8c8e821012133f20b6708ef1b6c378674627a67e
Ilia Tikhomirov <rbkshadow@gmail.com>
2018-11-22 09:57:59 +1100

    Initial Commit

---
+++ ResearchKit-Hands-on/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResearchKit-Hands-on
+//
+//  Created by Tikhomirov, Elliot (H&B, Marsfield) on 22/11/18.
+//  Copyright © 2018 Tikhomirov, Elliot (H&B, Marsfield). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
