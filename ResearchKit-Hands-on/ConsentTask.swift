2ec8cd08be49aa1759dc5c1d2abd29ec5dbf0207
Ilia Tikhomirov <rbkshadow@gmail.com>
2018-11-22 14:47:36 +1100

    Prepare the proof of concept for a modular researchkit app

--- ResearchKit-Hands-on/ConsentTask.swift
+++ ResearchKit-Hands-on/ConsentTask.swift
@@ -8,6 +8,32 @@
 
 import ResearchKit
 
+class ConsentTaskInitialiser {
+    static func createConsentTask(params: [String:String]) -> ORKOrderedTask {
+        var testTask: ORKOrderedTask {
+            var steps = [ORKStep]()
+            
+            //Add VisualConsentStep
+            var consentDocument = ConsentDocument
+            let visualConsentStep = ORKVisualConsentStep(identifier: "VisualConsentStep", document: consentDocument)
+            steps += [visualConsentStep]
+            
+            //Add ConsentReviewStep
+            let signature = consentDocument.signatures!.first as! ORKConsentSignature
+            
+            let reviewConsentStep = ORKConsentReviewStep(identifier: "ConsentReviewStep", signature: signature, in: consentDocument)
+            
+            reviewConsentStep.text = "TEST!"
+            reviewConsentStep.reasonForConsent = "TEST"
+            
+            steps += [reviewConsentStep]
+            
+            return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
+        }
+        return testTask
+    }
+}
+
 public var ConsentTask: ORKOrderedTask {
     
     var steps = [ORKStep]()
---
+++ ResearchKit-Hands-on/ProofOfConceptViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ProofOfConceptViewController.swift
+//  ResearchKit-Hands-on
+//
+//  Created by Tikhomirov, Elliot (H&B, Marsfield) on 22/11/18.
+//  Copyright © 2018 Tikhomirov, Elliot (H&B, Marsfield). All rights reserved.
+//
+
+import UIKit
+import ResearchKit
+
+class ProofOfConceptViewController: UIViewController, ORKTaskViewControllerDelegate {
+    
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        taskViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    var selection = 0
+    var collection = [ORKTaskViewController]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let taskViewController1 = ORKTaskViewController(task: ConsentTaskInitialiser.createConsentTask(params: [:]), taskRun: nil)
+        let taskViewController2 = ORKTaskViewController(task: SurveyTask, taskRun: nil)
+        collection.append(taskViewController1)
+        collection.append(taskViewController2)
+    }
+    
+    @IBAction func openSelectedDidTouch(_ sender: Any) {
+        collection[selection].delegate = self
+        present(collection[selection], animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBAction func indexChanged(_ sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            //textLabel.text = "First Segment Selected";
+            print("1")
+            selection = 0
+        case 1:
+            //textLabel.text = "Second Segment Selected";
+            print("2")
+            selection = 1
+        default:
+            break
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
